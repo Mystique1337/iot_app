85e2ea1d1ed83ce4de466a6bb61f4e24bf0c4774
Emmanuel Ashinze <66253355+Mystique1337@users.noreply.github.com>
2024-06-14 18:56:11 +0100

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ data = pd.read_csv(file_path)
 data.drop('Unnamed: 0', axis=1, inplace=True)
 # Convert date_surveyed to datetime
 data['date_surveyed'] = pd.to_datetime(data['date_surveyed'], format='%Y-%m-%d')
-data.rename(columns={'daily_co2_emmission_ppm_stationary': 'daily_co2_emmission_ppm_'}, inplace=True)
+data.rename(columns={'daily_co2_emmission_ppm_stationary': 'daily_co2_emmission_ppm'}, inplace=True)
 
 # Title for Streamlit app
 st.title("CO2 Emission Dashboard per Location")
